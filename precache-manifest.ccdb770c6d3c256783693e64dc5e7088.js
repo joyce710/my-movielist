1bf8bd801dfbfee3da6fb1bdd6d0561771529bf2
Joyce <joyce0x611@gmail.com>
2019-09-17 16:34:42 -0700

    Updates

---
+++ precache-manifest.ccdb770c6d3c256783693e64dc5e7088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007f9dae0622e61eb1a4c30ba62dd9b0",
+    "url": "/my-movielist/index.html"
+  },
+  {
+    "revision": "861619f29282042dc984",
+    "url": "/my-movielist/static/css/main.5b00a1b6.chunk.css"
+  },
+  {
+    "revision": "1259e926464c4f8d8cac",
+    "url": "/my-movielist/static/js/2.06b12f39.chunk.js"
+  },
+  {
+    "revision": "861619f29282042dc984",
+    "url": "/my-movielist/static/js/main.b9078c3a.chunk.js"
+  },
+  {
+    "revision": "1c8fc7a2542ce4382c59",
+    "url": "/my-movielist/static/js/runtime~main.6e6106d4.js"
+  },
+  {
+    "revision": "47bc979cb49b94d76338d9a520357edf",
+    "url": "/my-movielist/static/media/Netflix.47bc979c.jpg"
+  }
+]);
